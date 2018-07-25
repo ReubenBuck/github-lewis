1f0f3fe6dbeab3e1c00265c157c2b695a3cd33c1
ReubenBuck <buckleyrm@missouri.edu>
2018-07-25 11:50:22 -0500

    changes

--- GVCFgeno.sh
+++ GVCFgeno.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #SBATCH -p BioCompute
+#SBATCH --account=biocommunity
 #SBATCH -J GVCFgeno
 #SBATCH --mem 50G
 #SBATCH -N1
 #SBATCH -n28
-#SBATCH -t 0-12:00
+#SBATCH -t 2-00:00
 #SBATCH -o gtGVCFgeno.o%j
 #SBATCH -e gtGVCFgeno.e%j
 
@@ -20,19 +21,19 @@
 REFPATH="/home/buckleyrm/storage.lyonslab/cat_ref/"
 REFNAME="Felis_catus_9.0.fa"
 # Path to where gvcfs are kept
-GVCFPATH="/home/buckleyrm/storage.lyonslab/big.test.preprocess/gvcf"
+GVCFPATH="/home/buckleyrm/storage.lyonslab/results/Felis_catus/gvcf/"
 # file containg a list of lab ids
-LISTPATH="/home/buckleyrm/storage.lyonslab/big.test.preprocess/vcf/"
+LISTPATH="/home/buckleyrm/storage.lyonslab/dom_cat_run/"
 LISTNAME="id.list"
-# name of the output vcf 
-OUTPATH="/home/buckleyrm/storage.lyonslab/big.test.preprocess/vcf"
-OUTNAME="big.test"
+# name of the output vcf
+OUTPATH="/home/buckleyrm/storage.lyonslab/users/buckleyrm/cats186/"
+OUTNAME="dom_cat_run_186"
 #------------------------------------------------------------------
 
 module load java/openjdk/java-1.8.0-openjdk
 module load gatk/gatk-3.8
 
-awk -v gvcf=$GVCFPATH '{print gvcf "/" $0 ".sort.markDup.realign.bam.g.vcf.gz"}' $LISTPATH/$LISTNAME > $LISTPATH/tmp.$LISTNAME
+awk -v gvcf=$GVCFPATH '{print gvcf "/" $0 ".g.vcf.gz"}' $LISTPATH/$LISTNAME > $LISTPATH/tmp.$LISTNAME
 
 java -Djava.io.tmpdir=$GVCFPATH/tmp -XX:ParallelGCThreads=2 -jar /cluster/software/gatk/gatk-3.8/GenomeAnalysisTK.jar \
 -nt 26 \
--- dl_sra.slurm.sh
+++ dl_sra.slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1  # used for MPI codes, otherwise leave at '1'
 #SBATCH --cpus-per-task=1 # cores per task
-#SBATCH --mem-per-cpu=8G  # memory per core (default is 1GB/core)
+#SBATCH --mem-per-cpu=64G  # memory per core (default is 1GB/core)
 #SBATCH --time 2-00:00  # days-hours:minutes
 #SBATCH --qos=normal
 #SBATCH --account=biocommunity  # investors will replace this with their account name
@@ -26,15 +26,28 @@
 
 ### NOTES
 
+
+
 # invoke with RUN_SHEET=<sra run sheet with header> sbatch --array=2-$(wc -l < <sra run sheet with header>) dl_sra.sh
 
 # set tmp dir for sra caching before starting, otherwise home will fill up and kill task
-TMP=$(pwd)
-mkdir -p $TMP/.tmp_ncbi
-echo "/repository/user/main/public/root = \"$TMP/.tmp_ncbi\"" > $HOME/.ncbi/user-settings.mkfg
 
-# load module before begining
-# module load sratoolkit/sratoolkit-2.8.1-2
+
+module load sratoolkit/sratoolkit-2.8.1-2
+
+
+sleep $((RANDOM %10))
+
+TMP=$(pwd)
+
+if [ -d $TMP/.tmp_ncbi ]
+then
+        echo "tmp_ncbi already exists, continuing"
+else
+        echo "tmp_ncbi does not exist, creating"
+        mkdir -p $TMP/.tmp_ncbi
+        echo "/repository/user/main/public/root = \"$TMP/.tmp_ncbi\"" > $HOME/.ncbi/user-settings.mkfg
+fi
 
 #-------------------------------------------------------------------------------
 
@@ -47,22 +60,23 @@ sleep $((RANDOM % 5))
 ROW=$SLURM_ARRAY_TASK_ID
 SM=$(cut -f 15 $RUN_SHEET | sed "${ROW}q;d")
 
-
-
-# create dir unless it already exists
-if [ -d $SM ]
-then
-	echo "$SM dir exists, continuing"
-else
-	echo "$SM dir does not exist, creating"
-	mkdir $SM
-fi
-
-# get library name
+# library name
 LIB=$(cut -f 8 $RUN_SHEET | sed "${ROW}q;d")
 
 # get run name
-RUN=$(cut -f 4 $RUN_SHEET | sed "${ROW}q;d")
+RUN=$(cut -f 14 $RUN_SHEET | sed "${ROW}q;d")
+
+
+# create dir unless it already exists
+
+if [ -d $SM/$LIB ]
+then
+        echo "$SM/$LIB dir exists, continuing"
+else
+        echo "$SM/$LIB dir does not exist, creating"
+        mkdir -p $SM/$LIB
+fi
+
 
 echo -e "\nbegin fastq dump on sample $SM, library $LIB, srr $RUN \n"
 
@@ -70,28 +84,27 @@ echo -e "\nbegin fastq dump on sample $SM, library $LIB, srr $RUN \n"
 # pull fastq RUN and store in ./sm/lib/
 fastq-dump --split-files --origfmt --gzip --outdir ./$SM/$LIB $RUN
 
-echo -e "\nfastq dump complete, renaming srr from $RUN to $LIB"
-mv ./$SM/$LIB/${RUN}_1.fastq.gz ./$SM/$LIB/${LIB}_1.fastq.gz
-mv ./$SM/$LIB/${RUN}_2.fastq.gz ./$SM/$LIB/${LIB}_2.fastq.gz
+#echo -e "\nfastq dump complete, renaming srr from $RUN to $LIB"
+#mv ./$SM/$LIB/${RUN}_1.fastq.gz ./$SM/$LIB/${LIB}_1.fastq.gz
+#mv ./$SM/$LIB/${RUN}_2.fastq.gz ./$SM/$LIB/${LIB}_2.fastq.gz
 
-echo -e "\nunzip read 1"
-gunzip ./$SM/$LIB/${LIB}_1.fastq.gz
-echo unzip done, split into runs
-split_library_by_run ./$SM/$LIB/${LIB}_1.fastq
-echo split done, rm original
-rm ./$SM/$LIB/${LIB}_1.fastq
-echo zip new run fastq files
-gzip ./$SM/$LIB/${LIB}*_1.fastq
+#echo -e "\nunzip read 1"
+#gunzip ./$SM/$LIB/${LIB}_1.fastq.gz
+#echo unzip done, split into runs
+#~/scripts/bin/split_library_by_run ./$SM/$LIB/${LIB}_1.fastq
+#echo split done, rm original
+#rm ./$SM/$LIB/${LIB}_1.fastq
+#echo zip new run fastq files
+#gzip ./$SM/$LIB/${LIB}*_1.fastq
 
-echo -e "\nunzip read 2"
-gunzip ./$SM/$LIB/${LIB}_2.fastq.gz
-echo unzip done, split into runs
-split_library_by_run ./$SM/$LIB/${LIB}_2.fastq
-echo split done, rm original
-rm ./$SM/$LIB/${LIB}_2.fastq
-echo zip new fun fastq files
-gzip ./$SM/$LIB/${LIB}*_2.fastq
+#echo -e "\nunzip read 2"
+#gunzip ./$SM/$LIB/${LIB}_2.fastq.gz
+#echo unzip done, split into runs
+#~/scripts/bin/split_library_by_run ./$SM/$LIB/${LIB}_2.fastq
+#echo split done, rm original
+#rm ./$SM/$LIB/${LIB}_2.fastq
+#echo zip new fun fastq files
+#gzip ./$SM/$LIB/${LIB}*_2.fastq
 
 
 echo "### Ending at: $(date) ###" #!/bin/bash
-
