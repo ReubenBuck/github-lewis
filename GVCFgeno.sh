0dca7e7d6c532b18a4146a9e460930816a084b02
ReubenBuck <buckleyrm@missouri.edu>
2018-07-26 12:44:46 -0500

    lewis config

--- GVCFgeno.sh
+++ GVCFgeno.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -p Interactive
-#SBATCH --account=general
+#SBATCH -p Lewis
+#SBATCH --account=biocommunity
 #SBATCH -J GVCFgeno
 #SBATCH --mem 60G
 #SBATCH -N1
-#SBATCH -n3
-#SBATCH -t 0-00:10
+#SBATCH -n22
+#SBATCH -t 2-00:00
 #SBATCH --output=gtGVCFgeno-%A_%a-%j.out
 
 ## notifications
@@ -25,7 +25,7 @@ GVCFPATH="/home/buckleyrm/storage.lyonslab/results/Felis_catus/gvcf/"
 LISTPATH="/home/buckleyrm/storage.lyonslab/dom_cat_run/"
 LISTNAME="id.list"
 # name of the output vcf
-OUTPATH="/home/buckleyrm/storage.lyonslab/users/buckleyrm/cats186/interactive/"
+OUTPATH="/home/buckleyrm/storage.lyonslab/users/buckleyrm/cats186/lewis/"
 OUTNAME="dom_cat_run_186"
 #------------------------------------------------------------------
 
@@ -41,7 +41,7 @@ TARGET=$(echo $TARGETS | cut -d " " -f $SLURM_ARRAY_TASK_ID)
 awk -v gvcf=$GVCFPATH '{print gvcf "/" $0 ".g.vcf.gz"}' $LISTPATH/$LISTNAME > $LISTPATH/tmp.${TARGET%\.intervals}.$LISTNAME
 
 java -Djava.io.tmpdir=$GVCFPATH/tmp -XX:ParallelGCThreads=2 -jar /cluster/software/gatk/gatk-3.8/GenomeAnalysisTK.jar \
--nt 1 \
+-nt 20 \
 -T GenotypeGVCFs \
 -R $REFPATH/$REFNAME \
 -V $LISTPATH/tmp.${TARGET%\.intervals}.$LISTNAME \
